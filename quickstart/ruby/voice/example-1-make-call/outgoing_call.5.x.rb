3e63545430b7421d8e7ce95fad90ad10c7609185
Acerevenue <60113660+Acerevenue@users.noreply.github.com>
2020-01-20 15:07:17 -0500

    Update outgoing_call.5.x.rb

--- quickstart/ruby/voice/example-1-make-call/outgoing_call.5.x.rb
+++ quickstart/ruby/voice/example-1-make-call/outgoing_call.5.x.rb
@@ -1,8 +1,8 @@
 require 'twilio-ruby'
 
 # Get your Account Sid and Auth Token from twilio.com/console
-account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-auth_token = 'your_auth_token'
+account_sid = '5131563151313515151361515131'
+auth_token = '1511651515131356464513'
 
 # set up a client to talk to the Twilio REST API
 @client = Twilio::REST::Client.new(account_sid, auth_token)
